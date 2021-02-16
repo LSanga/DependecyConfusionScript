f31f7cd020d7dccb03106e2ab4648e3cc76dc918
LSanga <51289040+LSanga@users.noreply.github.com>
2021-02-16 21:46:56 +0000

    Update check-github-npm.py

--- github-npm/check-github-npm.py
+++ github-npm/check-github-npm.py
@@ -21,25 +21,23 @@ if args.target:
     
 
 #extract all package.json file from an organization on Github
-extract_packages = 'curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/search/code?q=JSON&q=org%3A'+organization+'+filename%3Apackage.json&type=Code" | jq -r ".items[].git_url"'
+extract_packages = 'curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/search/code?q=JSON&q=org%3A'+organization+'+filename%3Apackage.json&type=Code" | jq -r ".items[].git_url"'
 p = subprocess.Popen(extract_packages, stdout=subprocess.PIPE, shell=True)
 (packages, err) = p.communicate()
 packages = packages.split()
 
 #for every package, find all dependecies
 for package in packages:
-    #package = package.replace('"','')
     #print (package)
     
     #extract all dependencies
-    find_dependencies = "curl -H 'Accept: application/vnd.github.v3.raw' '"+str(package)+"' |  jq -r '.dependencies,.devDependencies | keys? | .[]'"
+    find_dependencies = "curl -s -H 'Accept: application/vnd.github.v3.raw' '"+str(package)+"' |  jq -r '.dependencies,.devDependencies | keys? | .[]'"
     
     p = subprocess.Popen(find_dependencies, stdout=subprocess.PIPE, shell=True)
     (dependencies, err) = p.communicate()
     dependencies = dependencies.split()
     
     for dependency in dependencies:
-        #dependency = dependency.replace('"','')
         #print (dependency)
         check_dependency = "npm info "+dependency+" 2>&1 > /dev/null || echo 404 && echo 200"
         p = subprocess.Popen(check_dependency, stdout=subprocess.PIPE, shell=True)
